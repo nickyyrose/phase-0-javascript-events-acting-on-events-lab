6a1baa5948ee7089f2506687271ec21d19cb9534
Nicholas Rose <ntrose90@gmail.com>
2024-03-10 07:46:31 -0700

    moving things with javascript

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.right = "0px";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < window.innerWidth - dodger.offsetWidth) {
+      left += 1;
+      dodger.style.left = `${left}px`;
+    }
+  }
